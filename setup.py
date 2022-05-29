b145eab62f84f4dcd293e8e7052705864d34747e
Indiandude123 <saheb123.singha@gmail.com>
2022-05-30 01:20:53 +0530

    pytest and tox added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src', 
+    version='0.0.1',
+    description='package to predict diabetes',
+    author='indiandude123',
+    packages=find_packages(),
+    license='MIT'
+)
